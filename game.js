c93e6623b08263676ce7ccb54bcb7459df8bcef4
Michael Fleshman <michaelfleshman90@gmail..com>
2018-07-12 23:57:33 -0400

    html update

--- game.js
+++ game.js
@@ -9,18 +9,26 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n",
 var i;
 var letterChosen = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-
 function counters() {
     if (playerGuess === letterChosen) {
         playerWins = playerWins + 1;
     } else {
         guessesLeft = guessesLeft - 1;
     }
+    if (guessesLeft === 0) {
+        playerLoses = playerLoses - 1;
+    }
 };
 function update() {
-    
+    if (playerGuess === letterChosen) {
+        text1 += "<br>Wins: " + 1;
+    } else {
+        text2 += "<br>Guesses left: " - 1;
+    }
 }
 // player guesses reaches 0 then reset game.
 if (playerGuesses === 0) {
     reset
 }
+document.getElementById("wins").innerHTML = text1;
+document.getElementById("guessesLeft").innerHTML = text2;
