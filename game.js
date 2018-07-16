7773681627714bac49806e37d4f51f3bbd0f105c
Michael Fleshman <michaelfleshman90@gmail..com>
2018-07-15 23:26:12 -0400

    small change

--- game.js
+++ game.js
@@ -4,42 +4,42 @@ var guessesLeft = 9;
 var lettersGuessed = [];
 var gameReset;
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length) + 1];
+
+for (var i = 9; i < guessesLeft; i--) {
+    document.onkeyup = function keylog(event) {
+        letterGuessed.push(event.key);
+        console.log(letterGuessed);
+        document.getElementById("guesses").innerHTML += event.key + ",";
+    };
+    keylog(event);
 
 
-document.onkeyup = function keylog(event) {
-    letterGuessed.push(event.key);
-    console.log(letterGuessed);
-    var x = letterGuessed;
-    document.getElementById("guesses").innerHTML = x;
-};
-keylog(event);
-
-
-//player win counter +1, else guesses left -1
-function counters() {
-    if (playerGuess === letterChosen) {
-        playerWins = playerWins + 1;
-    } else {
-        guessesLeft = guessesLeft - 1;
-    }
-    //player lose if guesses =0
-    if (guessesLeft === 0) {
-        playerLoses = playerLoses - 1;
-    }
-};
-
+    //player win counter +1, else guesses left -1
+    function counters() {
+        if (lettersGuessed === computerGuess) {
+            playerWins = playerWins + 1;
+            document.getElementById("wins").innerHTML = + 1;
+        } else {
+            guessesLeft = guessesLeft - 1;
+            document.getElementById("guessesLeft").innerHTML = - 1;
+        }
+        //player lose if guesses =0
+        if (guessesLeft === 0) {
+            playerLoses = playerLoses + 1;
+            document.getElementById("loses").innerHTML = + 1;
+        }
+    };
+}
 //document.getElementById("#wins").innerHTML = playerWins;
 //document.getElementById("#guessesLeft").innerHTML = guessesLeft;
 //document.getElementById("#loses").innerHTML = playerLoses;
-//if player guess isnt right push letter into lettersguessed array
-function update() {
-    
-}
+
+
 // player guesses reaches 0 then reset game.
 if (guessesLeft === 0) {
     gameReset
 }
-                                    //would it be easier to call the function and have it write it on the html?
+                                    
 
 
