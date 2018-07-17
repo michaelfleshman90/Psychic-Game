3a58e04ab3b7367967310e1190a8d90dfbb1c713
Michael Fleshman <michaelfleshman90@gmail..com>
2018-07-16 21:11:32 -0400

    finalize

--- game.js
+++ game.js
@@ -1,45 +1,68 @@
-var playerWins;
-var playerLoses;
+var playerWins = 0;
+var playerLoses = 0;
 var guessesLeft = 9;
-var lettersGuessed = [];
+var letterGuessed = [];
 var gameReset;
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length) + 1];
-
-for (var i = 9; i < guessesLeft; i--) {
-    document.onkeyup = function keylog(event) {
-        letterGuessed.push(event.key);
-        console.log(letterGuessed);
-        document.getElementById("guesses").innerHTML += event.key + ",";
-    };
-    keylog(event);
-
-
-    //player win counter +1, else guesses left -1
-    function counters() {
-        if (lettersGuessed === computerGuess) {
-            playerWins = playerWins + 1;
-            document.getElementById("wins").innerHTML = + 1;
-        } else {
-            guessesLeft = guessesLeft - 1;
-            document.getElementById("guessesLeft").innerHTML = - 1;
-        }
-        //player lose if guesses =0
-        if (guessesLeft === 0) {
-            playerLoses = playerLoses + 1;
-            document.getElementById("loses").innerHTML = + 1;
-        }
-    };
+    //key up event function
+document.addEventListener("keyup", function(event) {
+    //push the guess into letterGuessed array
+    letterGuessed.push(event.key);
+    //adds the guess to the html page
+    document.getElementById("guesses").innerHTML += event.key + ",";
+    //calls the counter function
+    counters(event.key);
+});
+//reset game
+function gameReset() {
+    //get another random number when game reset
+    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length) + 1];
+    //reset guesses to 9
+    guessesLeft = 9;
+    //updates wins if player wins
+    document.getElementById("wins").innerHTML = playerWins;
+    //updates guesses left 
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    //updates loses if player loses
+    document.getElementById("loses").innerHTML = playerLoses;
+    //resets guesses
+    document.getElementById("guesses").innerHTML = "";
 }
-//document.getElementById("#wins").innerHTML = playerWins;
-//document.getElementById("#guessesLeft").innerHTML = guessesLeft;
-//document.getElementById("#loses").innerHTML = playerLoses;
+    
+//counter function for wins loses and guessesleft
+function counters(letter) {
+    if (letter == computerGuess) {
+        playerWins = playerWins + 1;
+        //update the wins if player wins
+        document.getElementById("wins").innerHTML = playerWins;
+        alert("you win");
+        //reset if game is won
+        gameReset();
+    } else {
+        //guesses count down
+        guessesLeft = guessesLeft - 1;
+        //updates the guesses 
+        document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    }
+    //player lose if guesses game lost
+    if (guessesLeft === 0) {
+        //player loses + 1
+        playerLoses = playerLoses + 1;
+        //updates the loses if the player loses
+        document.getElementById("loses").innerHTML = playerLoses;
+        alert("You lose\n the letter was " + computerGuess);
+        //reset if player guesses =0
+        gameReset();
+    }
+    };
 
 
-// player guesses reaches 0 then reset game.
-if (guessesLeft === 0) {
-    gameReset
-}
+
+
+
+
+
                                     
 
 
